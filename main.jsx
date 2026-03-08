ba575976fb631fd096385761645309aec2045afb
bdanneman <brittcdanneman@gmail.com>
2026-03-08 15:36:12 -0700

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,5 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+  <rect width="32" height="32" fill="#0a0e14"/>
+  <polygon points="16,4 28,16 16,28 4,16" fill="none" stroke="#c8a050" stroke-width="1.5"/>
+  <text x="16" y="20" text-anchor="middle" font-size="12" fill="#c8a050" font-family="serif">✦</text>
+</svg>
